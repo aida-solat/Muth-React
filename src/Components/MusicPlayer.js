841c9b962d2b8f51694a7231664519d2befc1398
aida-solat <aidasolatco@gmail.com>
2022-07-04 22:38:42 +0300

    fix isshue

--- src/Components/MusicPlayer.js
+++ src/Components/MusicPlayer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   FaRegHeart,
   FaHeart,
@@ -15,21 +15,39 @@ import { BsDownload } from "react-icons/bs";
 function MusicPlayer(props) {
   const [isLove, setLoved] = React.useState(false);
   const [isPlay, setPlay] = React.useState(false);
+  const [duration, setDuration] = React.useState(0);
+
+  const audioPlayer = React.useRef();
+  const progressBar = React.useRef();
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+
+    setDuration(seconds);
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer.current.readyState]);
 
   const changeLoved = () => {
     setLoved(!isLove);
   };
 
   const changePlaying = () => {
-    setPlay(!isPlay);
+    const prevValue = isPlay;
+    if (!prevValue) {
+      audioPlayer.current.play();
+    }
+    audioPlayer.current.pause();
+
+    setPlay(!prevValue);
   };
 
   return (
     <>
       <div className="musicPlayer">
-        <div className="songImg">{props.imgSrc}</div>
+        <div className="songImg">
+          <img src={props.imgSrc} alt="song" />
+        </div>
         <div className="songAttr">
-          <audio src={props.song} preload="metadata" />
+          <audio src={props.song} preload="metadata" ref={audioPlayer} />
 
           <div className="top">
             <div className="left">
@@ -87,8 +105,8 @@ function MusicPlayer(props) {
           </div>
           <div className="bottom">
             <div className="currentTime">00:00:00</div>
-            <input type="range" className="progressBar" />
-            <div className="totalTime">00:00:00</div>
+            <input type="range" className="progressBar" ref={progressBar} />
+            <div className="totalTime">{duration}</div>
           </div>
         </div>
       </div>
